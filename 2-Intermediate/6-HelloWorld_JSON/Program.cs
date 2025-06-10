4c75c7dc612ba01c00bdd70b331dcdfcfd2a4831
Poom Yoosorn <poom.yoosorn@gmail.com>
2025-06-10 23:02:05 +0700

    C#Intermediate_6-HelloWorld_JSON

---
+++ 2-Intermediate/6-HelloWorld_JSON/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.Text.Json;
+using Dapper;
+using HelloWorld.Data;
+using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            DataContextDapper dapper = new DataContextDapper(config);
+
+            //-------------------------------------------------------
+            string computersJson = File.ReadAllText("Computers.json");
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            IEnumerable<Computer>? computersNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+            IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+
+
+            if (computersNewtonSoft != null)
+            {
+                foreach (Computer computer in computersNewtonSoft)
+                {
+                    //Console.WriteLine(computer.Motherboard);
+                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                        Motherboard,
+                        HasWifi,
+                        HasLTE,
+                        ReleaseDate,
+                        Price,
+                        VideoCard
+                    ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
+                            + "','" + computer.HasWifi
+                            + "','" + computer.HasLTE
+                            + "','" + computer.ReleaseDate
+                            + "','" + computer.Price
+                            + "','" + EscapeSingleQuote(computer.VideoCard)
+                    + "')";
+
+                    dapper.ExecuteSql(sql);
+                }
+            }
+
+            //-------------------------------------------
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonSoft, settings);
+            File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+
+            string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem, options);
+            File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+        }
+
+        static string EscapeSingleQuote(string input)
+        {
+            string output = input.Replace("'", "''");
+
+            return output;
+        }
+
+    }
+}
---
+++ 2-Intermediate/6-HelloWorld_JSON/SQL/SQLQuery_1.sql
@@ -0,0 +1,8 @@
+USE DotNetCourseDatabase
+GO
+
+SELECT * FROM  TutorialAppSchema.Computer;
+SELECT * FROM  TutorialAppSchema.Computer WHERE VideoCard = 'Robel-O''Hara';
+
+
+TRUNCATE TABLE TutorialAppSchema.Computer;
