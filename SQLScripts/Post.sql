18eddc55b62f937950b4c0ccbf66539a8d0a8260
Poom Yoosorn <poom.yoosorn@gmail.com>
2025-06-22 10:41:24 +0700

    API_Intermediate_11-DotnetAPI_RelatedData

--- SQLScripts/Post.sql
+++ SQLScripts/Post.sql
@@ -10,3 +10,26 @@ CREATE TABLE TutorialAppSchema.Posts (
 )
 
 CREATE CLUSTERED INDEX cix_Posts_UserId_PostId ON TutorialAppSchema.Posts(UserId, PostId)
+
+SELECT [PostId],
+[UserId],
+[PostTitle],
+[PostContent],
+[PostCreated],
+[PostUpdated] FROM TutorialAppSchema.Posts
+
+INSERT INTO TutorialAppSchema.Posts(
+                [UserId],
+                [PostTitle],
+                [PostContent],
+                [PostCreated],
+                [PostUpdated]) VALUES (,'','', GETDATE(), GETDATE())
+
+
+UPDATE TutorialAppSchema.Posts 
+                    SET UserId = 1
+                    WHERE PostId = 1
+
+
+SELECT * FROM TutorialAppSchema.Posts
+    WHERE PostTitle LIKE '%Poqom%' OR PostContent LIKE '%Poom%'
